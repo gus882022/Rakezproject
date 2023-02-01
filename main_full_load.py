18db6da16c9dbd271e9c26fcb064a9f644dc70b4
gus882003@gmail.com <gus882003@gmail.com>
2023-02-01 07:45:10 +0000

    Commit 2 01/02/2023

--- main_full_load.py
+++ main_full_load.py
@@ -38,30 +38,29 @@ def create_database_catalog():
 # COMMAND ----------
 
 def main():
-    #try:
-    # Send start notification 
-    subject=f"Start Process Date {date_process['today']}"    
-    msg=f"""The process for day {date_process['today']} has started"""
-    send_notification(subject=subject,msg=msg)
+    try:
+        # Send start notification 
+        subject=f"Start Process Date {date_process['today']}"    
+        msg=f"""The process for day {date_process['today']} has started"""
+        send_notification(subject=subject,msg=msg)
 
-    # create databases into databricks system
-    create_database_catalog()
-    dbutils.notebook.run("notebooks/POKRakezIngestRawFullLoadsparkSQL",0)
-    dbutils.notebook.run("notebooks/POKRakezIngestRawFullLoadsparkSQL",0)
-    dbutils.notebook.run("notebooks/POKRakezIngestRawFullLoadsparkSQL",0)
+        # create databases into databricks system
+        create_database_catalog()
+        dbutils.notebook.run("notebooks/POKRakezIngestRawFullLoadsparkSQL",0)
+        dbutils.notebook.run("notebooks/POKRakezDQRules",0)
 
 
-    # Send finish notification 
+        # Send finish notification 
 
-    subject=f"Finish Process Date {date_process['today']}"    
-    msg=f"""The process for day {date_process['today']} has finished"""
-    send_notification(subject=subject,msg=msg)
+        subject=f"Finish Process Date {date_process['today']}"    
+        msg=f"""The process for day {date_process['today']} has finished"""
+        send_notification(subject=subject,msg=msg)
+
         
-        
-    #except Exception as e:
-    #    subject=f"Error log date {date_process['today']}"        
-    #    msg=f"""Error creating databases in databricks please review ERROR is: {e}"""
-    #    send_notification(subject=subject,msg=msg)
+    except Exception as e:
+        subject=f"Error log date {date_process['today']}"        
+        msg=f"""Error creating databases in databricks please review ERROR is: {e}"""
+        send_notification(subject=subject,msg=msg)
         
 
 # COMMAND ----------
@@ -72,63 +71,6 @@ def main():
 # COMMAND ----------
 
 main()
-
-# COMMAND ----------
-
-for line in dbutils.fs.ls('s3://demodmsgbraw/candidate/'):
-    id_execution=line
-    print(line[1])
-
-# COMMAND ----------
-
-df.write.format("delta").mode("overwrite").option("path","s3://demodmsgbsilver/customers/").saveAsTable(f"{databases['silver']}.customers")
-
-# COMMAND ----------
-
-df.createOrReplaceTempView('customers')
-
-# COMMAND ----------
-
-query=f"""
-        create table if not exists {databases['silver']}.customers
-        using delta
-        location 's3://demodmsgbsilver/customers/'
-        as
-        select *
-        from customers
-"""
-spark.sql(query)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC use  silver_data_rakez;
-# MAGIC 
-# MAGIC show tables;
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from candidate
-
-# COMMAND ----------
-
-send_notification(msg="Hola",subject="Hola")
-
-# COMMAND ----------
-
-dbutils.fs.cp('s3a://demodmsgbraw/candidate/','s3a://demodmsgbraw/processed/candidate',True)
-
-# COMMAND ----------
-
-dbutils.notebook.run("notebooks/POKRakezIngestRawFullLoadsparkSQL",0)
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC use silver_data_rakez;
-# MAGIC 
-# MAGIC show tables
 
 # COMMAND ----------
 
